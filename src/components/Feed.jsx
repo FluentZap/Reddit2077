15f54bf1a33b0df7314ff47ed4516764ba522a14
Stuart McKay and Todd Aden <stuart+todd@>
2019-07-09 15:14:57 -0700

    upvate and downvote state change

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -4,68 +4,50 @@ import { v4 } from 'uuid';
 
 export default function Feed() {
 
-  const [posts, setPost] = useState({
+  let [posts, setPost] = useState([{
     number: 1,
     count: 1,
     title: "This is my first post on Reddit. Please be gentle.",
     image: '',
     id: v4()
   },
-  // {
-  //   number: 2,
-  //   count: 1,
-  //   title: "Feeling cute rn, might delete late. idk.",
-  //   image: '',
-  //   id: v4()
-  // },
-  // {
-  //   number: 3,
-  //   count: 1,
-  //   title: "DAE every breath air? TIL",
-  //   image: '',
-  //   id: v4()
-  // }]
-  );
-
-  function upvote(index, newposts) {
-    newposts[index].count++;
-    setPost(newposts);
-  }
-
-  function downvote() {
-
-  }
+  {
+    number: 2,
+    count: 1,
+    title: "Feeling cute rn, might delete later. idk.",
+    image: '',
+    id: v4()
+  },
+  {
+    number: 3,
+    count: 1,
+    title: "DAE ever breath air? TIL",
+    image: '',
+    id: v4()
+  }]
+  );  
+  
 
   return (
     <div>
+      {posts.map((newPost, index) =>
         <div>
-          <button onClick={() => setPost(prevState => {
-            return {...prevState, count: prevState.count + 1}
+          <button onClick={() => setPost(prevState => {   
+            let newState = [...prevState];
+            newState[index].count++;
+            return newState;
           })}>
             Upvote
           </button>
-          {/* <button onClick={() => setPost(posts[index].count - 1)}>
-            Downvote
-          </button> */}
-          <Post
-            number={posts.number}
-            count={posts.count}
-            title={posts.title}
-            image={posts.image}
-            key={posts.id}
-          />
-        </div>
-      
-      {/* {posts.map((newPost, index) =>
-        <div>
-          <button onClick={() => setPost(prevState => {
-            return {...prevState, count: prevState.count + 1}
+
+          <button onClick={() => setPost(prevState => {   
+            let newState = [...prevState];
+            newState[index].count--;
+            return newState;
           })}>
-            Upvote
-          </button>
-          <button onClick={() => setPost(posts[index].count - 1)}>
             Downvote
           </button>
+
           <Post
             number={newPost.number}
             count={newPost.count}
@@ -74,7 +56,7 @@ export default function Feed() {
             key={newPost.id}
           />
         </div>
-      )}; */}
+      )};
     </div>
   )
 }
